dff313f9faa15a80b083aa03e255cad1bd8a0c64
RgnDunes <divyansh.singh@razorpay.com>
2024-10-17 22:31:18 +0530

    add session 8 code

---
+++ Session 08/basics.js
@@ -0,0 +1,18 @@
+// Event Object
+/*
+--> Mouse Events: click, dblclick, mousedown, mouseup, mouseover, mouseout, mousemove
+--> Keyboard Events: keydown, keyup, keypress
+--> Form Events: submit, focus, blur, change
+--> Document Events: DOMContentLoaded
+--> Window Events: load, resize, scroll
+*/
+
+// Event Propagation
+/*
+Three Phases:
+--> Capturing Phase
+--> Target Phase
+--> Bubbling Phase
+*/
+
+// Event Delegation
