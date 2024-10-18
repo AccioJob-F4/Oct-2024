87e6f9040b6d3c623f139a888e5ce34bdd821a76
RgnDunes <divyansh.singh@razorpay.com>
2024-10-18 19:14:06 +0530

    add assignment solution

---
+++ assignments/State Management Basics/QnA.js
@@ -0,0 +1,1 @@
+// MCQs? Easy peasy lemon squeezy! 🍋😆 Go ahead and tackle them all by yourself. Have fun! 🎉📚😉
---
+++ assignments/Working with APIs/GettersandSetters.js
@@ -0,0 +1,42 @@
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+
+  // Setter for age
+  set age(age) {
+    if (typeof age === "number" && age >= 0) {
+      this._age = age;
+    } else {
+      console.error("Please provide a valid age.");
+    }
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
+
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
